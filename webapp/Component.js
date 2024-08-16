f03d6ce4a2509d019bdde28ecd2bfb3eb945ea89
Sonu Chouhan <sonu.chouhan@in.ey.com>
2024-08-16 09:04:41 +0000

    my stable changes a

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("ats.sc.travelproces.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
